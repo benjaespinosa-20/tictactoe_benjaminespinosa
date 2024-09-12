5d20fa1a1c772edcb72e7b58678dc144641e6a3c
Karen <elenavilezv@hotmail.com>
2024-09-11 18:23:09 -0600

    Change strings

---
+++ src/main/kotlin/VisionBoard.java
@@ -0,0 +1,5 @@
+public class VisionBoard {
+    public static void main(String[] args) {
+        System.out.println("Te amo niño precioso");
+    }
+}
--- src/main/kotlin/visionboard.java
+++
@@ -1,5 +0,0 @@
-public class visionboard {
-    public static void main(String[] args) {
-        System.out.println("te amo niño precioso");
-    }
-}
